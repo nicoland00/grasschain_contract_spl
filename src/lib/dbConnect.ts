0f1200694fea60f61e53f165088230df3648fb53
nicoland00 <nicoland00@example.com>
2025-04-30 01:35:55 +0200

    production ready 5

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,20 +1,23 @@
-// /src/lib/dbConnect.ts
+// src/lib/dbConnect.ts
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
-if (!MONGODB_URI) throw new Error("⚠️ Define la variable MONGODB_URI en tu .env");
+let cached: { conn: mongoose.Mongoose | null; promise: Promise<mongoose.Mongoose> | null };
 
-let cached = (global as any).mongoose;
-if (!cached) {
-  cached = (global as any).mongoose = { conn: null, promise: null };
-}
-
+// We don’t grab the URI until runtime:
 export async function dbConnect() {
-  if (cached.conn) return cached.conn;
+  const MONGODB_URI = process.env.MONGODB_URI;
+  if (!MONGODB_URI) {
+    throw new Error("⚠️ Define la variable MONGODB_URI en tu .env or in Vercel settings.");
+  }
+
+  if (!cached) {
+    cached = { conn: null, promise: null };
+  }
+  if (cached.conn) {
+    return cached.conn;
+  }
   if (!cached.promise) {
-    cached.promise = mongoose
-      .connect(MONGODB_URI as string)
-      .then((mongoose) => mongoose);
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
   }
   cached.conn = await cached.promise;
   return cached.conn;
